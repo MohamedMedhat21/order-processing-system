9156789b3a1395e966fdbea4bc9d6baa9d375215
mohamed medhat <mohamedmmedhat21@gmail.com>
2026-06-28 10:17:08 +0300

    ci file permissions

--- src/test/java/io/github/mohamedmedhat21/order_processing_system/TestcontainersConfiguration.java
+++ src/test/java/io/github/mohamedmedhat21/order_processing_system/TestcontainersConfiguration.java
@@ -13,7 +13,7 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:18"));
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine"));
 	}
 
 	@Bean
